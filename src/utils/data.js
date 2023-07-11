9da8985324ac7248da0041fcec9c0189554ff201
sunil <neil001002@gmail.com>
2023-07-11 17:44:12 +0530

    initial commit

---
+++ src/utils/data.js
@@ -0,0 +1,49 @@
+import I1Img from "../assets/i1.png";
+import F1Img from "../assets/f1.png";
+import C3Img from "../assets/c1.png";
+import Fi1Img from "../assets/fi1.png";
+
+export const heroData = [
+  { id: 1, name: "Icecream", decp: "Choclate & Vanilla", price: "5.25", imgSrc: I1Img },
+  { id: 2, name: "Strawberries", decp: "Fresh Strawberries", price: "10.25", imgSrc: F1Img },
+  { id: 3, name: "Chicken Kebab", decp: "Mixed Kebab Plate", price: "8.25", imgSrc: C3Img },
+  { id: 4, name: "Fish Kebab", decp: "Mixed Fish Kebab", price: "5.25", imgSrc: Fi1Img },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Chicken",
+    urlParamName: "chicken",
+  },
+  {
+    id: 2,
+    name: "Curry",
+    urlParamName: "curry",
+  },
+  {
+    id: 3,
+    name: "Rice",
+    urlParamName: "rice",
+  },
+  {
+    id: 4,
+    name: "Fish",
+    urlParamName: "fish",
+  },
+  {
+    id: 5,
+    name: "Fruits",
+    urlParamName: "fruits",
+  },
+  {
+    id: 6,
+    name: "Icecreams",
+    urlParamName: "icecreams",
+  },
+  {
+    id: 7,
+    name: "Soft Drinks",
+    urlParamName: "drinks",
+  },
+];
